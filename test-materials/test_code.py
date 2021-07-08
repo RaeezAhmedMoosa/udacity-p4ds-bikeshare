56aa9630062dd0f6a0405af7543d5532e60dd25d
Raeez <raeez.burner@gmail.com>
2021-07-08 13:27:15 +0200

    test: Access New York City data for DataFrame creation

--- test-materials/test_code.py
+++ test-materials/test_code.py
@@ -1,14 +1,30 @@
 # This file will be used to write all the initial code for the "Bikeshare" project
 # This file will not be included in the final version of the project code
 import numpy as np
-import os
 import pandas as pd
 
 # File Directory Tests
+
+# Chicago
 ch_data = "./chicago.csv"
 
 ch_data1 = "../bikeshare-data/chicago.csv"
 
 ch_df = pd.read_csv(ch_data)
 
-os.getcwd()
+type(ch_df)
+
+
+
+
+# New York City
+nyc_data = "./new_york_city.csv"
+
+nyc_data1 = "../bikeshare-data/new_york_city.csv"
+
+nyc_df = pd.read_csv(nyc_data1)
+
+type(nyc_df)
+
+
+# Washington
--- test-materials/test_script.py
+++ test-materials/test_script.py
@@ -2,14 +2,13 @@
 # "test_code" file will be run. This file will not be included in the final
 # version of the "Bikeshare" project.
 import numpy as np
-import os
 import pandas as pd
 
 # File Directory Tests
-ch_data = "./chicago.csv"
+nyc_data = "./new_york_city.csv"
 
-ch_data1 = "../bikeshare-data/chicago.csv"
+nyc_data1 = "../bikeshare-data/new_york_city.csv"
 
-ch_df = pd.read_csv(ch_data1)
+nyc_df = pd.read_csv(nyc_data1)
 
-print(type(ch_df))
+print(type(nyc_df))
