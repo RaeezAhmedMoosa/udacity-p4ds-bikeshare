691ce767f15e256af5a897a939401d3624dba843
Raeez <raeez.burner@gmail.com>
2021-07-08 15:59:31 +0200

    feat: Add code for Practice Problem 1 solution

--- test-materials/test_code.py
+++ test-materials/test_code.py
@@ -76,3 +76,49 @@ ch_df["Start Station"].unique()
 
 # This returns the number of unique Starting Stations in the Chicago dataset
 len(ch_df["Start Station"].unique())
+
+
+
+
+# Practice Problem 1
+#
+# Use pandas to load chicago.csv into a dataframe, and find the most frequent
+# hour when people start traveling. There isn't an hour column in this dataset,
+# but you can create one by extracting the hour from the "Start Time" column.
+# To do this, you can convert "Start Time" to the datetime datatype using the
+# pandas to_datetime() method and extracting properties such as the hour with
+# these properties.
+#
+# Hint: Another way to describe the most common value in a column is the mode.
+ch_df = pd.read_csv("../bikeshare-data/chicago.csv")
+
+# Confirming that ch_df is a DataFrame object
+type(ch_df)
+
+# Checking the column names
+ch_df.columns
+
+# Viewing 5 rows of the "Start Time" column
+ch_df["Start Time"].head()
+
+# Verifying that "Start Time" is an object data type
+ch_df.info()
+
+# Viewing the first row of the "Start Time" column
+ch_df["Start Time"][0]
+
+# Obtaining the length of the characters in the first row of the "Start Time" column
+len(ch_df["Start Time"][0])
+
+# Extracting a slice of the value in the first row of the "Start Time" column
+ch_df["Start Time"][0][11:13]
+
+# This converts "Start Time" into the datetime64 data type
+ch_df["Start Time"] = pd.to_datetime(ch_df["Start Time"])
+
+# This creates a new column "Hour" by extracting the hour values from the "Start
+#  Time" column
+ch_df["Hour"] = ch_df["Start Time"].dt.hour
+
+# This returns the modal value for the "Hours" column
+popular_hour = ch_df["Hour"].mode()[0]
--- test-materials/test_script.py
+++ test-materials/test_script.py
@@ -4,25 +4,23 @@
 import numpy as np
 import pandas as pd
 
-# File Directory Tests
-ch_data1 = "../bikeshare-data/chicago.csv"
+# Practice Problem 1
+ch_df = pd.read_csv("../bikeshare-data/chicago.csv")
 
-ch_df = pd.read_csv(ch_data1)
+#print(ch_df.info())
+
+ch_df["Start Time"] = pd.to_datetime(ch_df["Start Time"])
+
+#print(ch_df.info())
+
+#print(ch_df["Start Time"].head())
+
+ch_df["Hour"] = ch_df["Start Time"].dt.hour
+
+#print(ch_df["Hour"].head())
 
 print(ch_df.head())
 
-print(ch_df.columns)
+popular_hour = ch_df["Hour"].mode()[0]
 
-print(ch_df.isnull().any())
-
-print(ch_df.describe())
-
-print(ch_df.info())
-
-print(ch_df["Gender"].value_counts())
-
-print(ch_df[["Gender", "User Type"]].value_counts())
-
-print(ch_df["Start Station"].unique())
-
-print(len(ch_df["Start Station"].unique()))
+print(popular_hour)
