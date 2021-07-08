3768e17eb0d11e706f0a8364da95794944d54713
Raeez <raeez.burner@gmail.com>
2021-07-08 12:17:23 +0200

    feat: Add testing files for project start

---
+++ test-materials/test_code.py
@@ -0,0 +1,2 @@
+# This file will be used to write all the initial code for the "Bikeshare" project
+# This file will not be included in the final version of the project code
---
+++ test-materials/test_script.py
@@ -0,0 +1,3 @@
+# This file will be used as a testing ground, from which code written in the
+# "test_code" file will be run. This file will not be included in the final
+# version of the "Bikeshare" project.
