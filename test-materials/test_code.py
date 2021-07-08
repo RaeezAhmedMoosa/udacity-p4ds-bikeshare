7430fbf480133c4df9c00ccd8c4d25938ae4edf9
Raeez <raeez.burner@gmail.com>
2021-07-08 13:19:58 +0200

    test: Access Chicago data file from separate directory

--- test-materials/test_code.py
+++ test-materials/test_code.py
@@ -1,10 +1,14 @@
 # This file will be used to write all the initial code for the "Bikeshare" project
 # This file will not be included in the final version of the project code
 import numpy as np
+import os
 import pandas as pd
 
 # File Directory Tests
-ch_data = "/c/Users/Raeez/Desktop/Udacity/3 - Programming for Data Science "
-          "with Python/1 - Projects/bikeshare/bikeshare-data/chicago.csv"
+ch_data = "./chicago.csv"
+
+ch_data1 = "../bikeshare-data/chicago.csv"
 
 ch_df = pd.read_csv(ch_data)
+
+os.getcwd()
--- test-materials/test_script.py
+++ test-materials/test_script.py
@@ -2,11 +2,14 @@
 # "test_code" file will be run. This file will not be included in the final
 # version of the "Bikeshare" project.
 import numpy as np
+import os
 import pandas as pd
 
 # File Directory Tests
 ch_data = "./chicago.csv"
 
-ch_df = pd.read_csv(ch_data)
+ch_data1 = "../bikeshare-data/chicago.csv"
+
+ch_df = pd.read_csv(ch_data1)
 
 print(type(ch_df))
