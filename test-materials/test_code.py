b20fb472a980dce262f3f85ef74b242a4d5264d0
Raeez <raeez.burner@gmail.com>
2021-07-08 12:59:16 +0200

    test: Access Chicago data file for DataFrame creation
    As a first step, this code is written to check and verify that the
    chicigo.csv is loaded correctly and that the accompanying DataFrame is
    created as a result.
    
    One issue to be looked at further is that I experienced problems in
    accessing the csv file when it is in a location outside of the directory
    that the test_script.py file is in.

--- test-materials/test_code.py
+++ test-materials/test_code.py
@@ -1,2 +1,10 @@
 # This file will be used to write all the initial code for the "Bikeshare" project
 # This file will not be included in the final version of the project code
+import numpy as np
+import pandas as pd
+
+# File Directory Tests
+ch_data = "/c/Users/Raeez/Desktop/Udacity/3 - Programming for Data Science "
+          "with Python/1 - Projects/bikeshare/bikeshare-data/chicago.csv"
+
+ch_df = pd.read_csv(ch_data)
--- test-materials/test_script.py
+++ test-materials/test_script.py
@@ -1,3 +1,12 @@
 # This file will be used as a testing ground, from which code written in the
 # "test_code" file will be run. This file will not be included in the final
 # version of the "Bikeshare" project.
+import numpy as np
+import pandas as pd
+
+# File Directory Tests
+ch_data = "./chicago.csv"
+
+ch_df = pd.read_csv(ch_data)
+
+print(type(ch_df))
