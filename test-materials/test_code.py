80e6b778f9533109b243fdf1e7d0f22a2c1b49a2
Raeez <raeez.burner@gmail.com>
2021-07-09 11:54:26 +0200

    feat: Add code for Practice Problem 3 - Step 1 solution

--- test-materials/test_code.py
+++ test-materials/test_code.py
@@ -150,3 +150,61 @@ type(users_count)
 # These integer locations return the relevant Indices  in the pandas Series
 users_count.iloc[0]
 users_count.iloc[:]
+
+
+
+
+# Practice Problem 3: Load and Filter the Dataset
+#
+# This is a bit of a bigger task, which involves choosing a dataset to load and
+# filtering it based on a specified month and day. In the quiz below, you'll
+# implement the load_data() function, which you can use directly in your project.
+#
+# There are four steps:
+#
+# 1. Load the dataset for the specified city. Index the global CITY_DATA dictionary
+#    object to get the corresponding filename for the given city name.
+
+# Step 1
+cities_data = {
+              "chicago" : "../bikeshare-data/chicago.csv",
+              "new york city" : "../bikeshare-data/new_york_city.csv",
+              "washington" : "../bikeshare-data/washington.csv"
+}
+
+# For Loop printing the Key Value pairs for the dictionary. Note the use of the
+# items() method to achieve this.
+for key, value in cities_data.items():
+    print(key, value)
+
+# For Loop printing the Keys from the dictionary. Note that when the dictionary
+# is used like this in a For Loop, with no method being called on it, the default
+# will be to return/print the Keys
+for key in cities_data:
+    print(key.title())
+
+# For Loop printint the dictionary Values. Note the use of the values() method
+# to achieve this
+for value in cities_data.values():
+    print(value)
+
+#
+def load_data(city):
+    """
+    Loads data for the specified city and filters by month and day if applicable.
+
+    Args:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - pandas DataFrame containing city data filtered by month and day
+    """
+    df = pd.read_csv(cities_data[city.lower()])
+    print("This is the DataFrame for:", city.title())
+    print(df.info())
+    print(df.head())
+    
+
+
+load_data("NEW YORK CITY")
--- test-materials/test_script.py
+++ test-materials/test_script.py
@@ -4,17 +4,28 @@
 import numpy as np
 import pandas as pd
 
-# Practice Problem 2
-filename = "../bikeshare-data/chicago.csv"
+# Practice Problem 3
+cities_data = {
+              "chicago" : "../bikeshare-data/chicago.csv",
+              "new york city" : "../bikeshare-data/new_york_city.csv",
+              "washington" : "../bikeshare-data/washington.csv"
+}
 
-df = pd.read_csv(filename)
+def load_data(city):
+    """
+    Loads data for the specified city and filters by month and day if applicable.
 
-print(type(df))
+    Args:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - pandas DataFrame containing city data filtered by month and day
+    """
+    df = pd.read_csv(cities_data[city.lower()])
+    print("This is the DataFrame for:", city.title())
+    print(df.info())
+    print(df.head())
 
-users_count = df["User Type"].value_counts()
 
-print(users_count)
-
-print(type(users_count))
-
-print(users_count.iloc[:])
+load_data("new york city")
