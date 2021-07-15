950d9a522ccf4972614a09512fae62c2b8fbbb7a
Raeez <raeez.burner@gmail.com>
2021-07-15 16:36:15 +0200

    feat: Create new Python file for Test Program

---
+++ test-materials/test_program.py
@@ -0,0 +1,7 @@
+# This file will be used to refactor the existing code from test_code.py and to
+# write new functions that might be needed as a result of the refactoring
+
+# Imported Libraries and Packages:
+import datetime as dt
+import numpy as np
+import pandas as pd
